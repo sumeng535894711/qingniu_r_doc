94c7bf09d1cdaa836c4c5cd5f274a77f3c46e7da
苏蒙 <535894711@qq.com>
2022-03-02 09:43:11 +0800

    Updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var ghpages = require("gh-pages");
 ghpages.publish(
   "_book",
   {
-    branch: "master",
+    branch: "main",
     repo: "https://github.com/sumeng535894711/qingniu_r_doc.git"
   },
   function(err) {
